e7d03f27a6fbd74e0d4b6a5534d4873ad803a73d
njxmi <nicole.jones.mi@gmail.com>
2017-07-18 15:21:17 -0400

    first commit, incomplete work

---
+++ script.js
@@ -0,0 +1,45 @@
+<script>
+
+
+
+var items = [ 
+	{name:'beans', 					price: 0.99},
+	{name: 'rice', 					price: 3.99},
+	{name: 'flour', 				price: 1.99},
+	{name: 'truffle oil', 			price: 7.25},
+	{name: 'spinach', 				price: 2.00},
+	{name: 'artichokes', 			price: 2.50},
+	{name: 'rotisserie chicken',	price: 1.99},
+	{name: 'almonds', 				price: 9.99},	];
+
+
+	function listGroceries(items) {
+  var itemName = "";
+  var itemPrice = 0;
+  groceries.forEach(function(item) {
+  itemName = items.name;
+  itemPrice = price.items;
+  }
+  );
+	}
+		  console.log( name + price );
+  
+	
+
+ 
+	
+	
+	
+/* Instructor's example code:
+for (var i = 0; i < numbers.length; i++) {
+   console.log( numbers[i] );
+ }function incrementNumbers(numbers) {
+ for (var i = 0; i < numbers.length; i++) {
+   numbers[i]++;
+ }
+}
+
+*/
+
+
+</script>
