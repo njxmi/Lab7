9e0c1948797037dc5593640bb150ea31270166ef
njxmi <nicole.jones.mi@gmail.com>
2017-07-19 08:50:09 -0400

    second commit

--- script.js
+++ script.js
@@ -1,45 +1,73 @@
-<script>
-
 
 
 var items = [ 
-	{name:'beans', 					price: 0.99},
-	{name: 'rice', 					price: 3.99},
-	{name: 'flour', 				price: 1.99},
-	{name: 'truffle oil', 			price: 7.25},
-	{name: 'spinach', 				price: 2.00},
-	{name: 'artichokes', 			price: 2.50},
-	{name: 'rotisserie chicken',	price: 1.99},
-	{name: 'almonds', 				price: 9.99},	];
+	{name: "beans", 					    price: 0.99},
+	{name: "rice", 					      price: 3.99},
+	{name: "flour", 				      price: 1.99},
+	{name: "truffle oil", 			  price: 7.25},
+	{name: "spinach", 				    price: 2.00},
+	{name: "artichokes", 			    price: 2.50},
+	{name: "rotisserie chicken",	price: 5.99},
+	{name: "almonds", 				    price: 9.99},	];
 
-
-	function listGroceries(items) {
+//original code
+/*	function listGroceries(items) {
   var itemName = "";
   var itemPrice = 0;
   groceries.forEach(function(item) {
   itemName = items.name;
-  itemPrice = price.items;
+  itemPrice = items.price;
   }
   );
 	}
-		  console.log( name + price );
+		  console.log(itemName + itemPrice);
+		  
+		  */
   
 	
+//trying to set up list of items, some of which are subject to sales tax
 
- 
-	
-	
-	
-/* Instructor's example code:
-for (var i = 0; i < numbers.length; i++) {
-   console.log( numbers[i] );
- }function incrementNumbers(numbers) {
- for (var i = 0; i < numbers.length; i++) {
-   numbers[i]++;
- }
+var totalBill = 0;
+
+
+totalCost += getPriceOfChicken(priceTax);
+
+console.log("Your total is $" + totalCost.toFixed(2));
+
+function getPriceOfChicken(isCooked, addTax) {
+    var priceTax;
+    if (isCooked) {
+        priceTax = 5.99;
+    } else {
+        priceTax = 3.99;
+    }
+    if (addTax) {
+        priceTax += (priceTax * 1.06);
+    }
+    return priceTax;
 }
 
-*/
 
 
-</script>
+function getPriceOfItem(choice) {
+  switch (choice) {
+      case "beans":
+		return 0.99;
+      case "rice":
+		return 3.99;
+      case "flour":
+		return 1.99
+      case "truffle oil":
+          return 7.25
+      case "spinach":
+		return 2.00
+      case "artichokes":
+          return 2.5;
+      case "almonds":
+          return 9.99
+  }
+}
+
+function calcTotal(subtotalNonTaxable){
+  return subtotalTaxable + ;  
+}
